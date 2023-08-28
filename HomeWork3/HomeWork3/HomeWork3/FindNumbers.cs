912168a370a7d37f5e5a768f7ceb3789cb5272eb
AlexArdas <asg270901@mail.ru>
2023-08-28 20:14:56 +0300

    Выполнил HomeWork3

---
+++ HomeWork3/HomeWork3/HomeWork3/FindNumbers.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HomeWork3
+{
+    /// <summary>
+    /// Класс для поиска и записи номеров телефонов
+    /// </summary>
+    public static class FindNumbers
+    {
+        /// <summary>
+        /// Находит номера телефонов в тексте, записывает их в файл и выводит на консоль
+        /// </summary>
+        public static void FindAndWritePhoneNumbers()
+        {
+            string inputFilePath = "Text.txt";
+            string outputFilePath = "Numbers.txt";
+
+            string inputText = File.ReadAllText(inputFilePath);
+
+            List<string> phoneNumbers = FindPhoneNumbers(inputText);
+
+            WritePhoneNumbersToFile(phoneNumbers, outputFilePath);
+
+            Console.WriteLine("\nНайденные номера телефонов:");
+            foreach (string phoneNumber in phoneNumbers)
+            {
+                Console.WriteLine(phoneNumber);
+            }
+        }
+
+        /// <summary>
+        /// Находит номера телефонов в тексте с использованием регулярных выражений
+        /// </summary>
+        /// <param name="text">Текст для поиска</param>
+        /// <returns>Список найденных номеров телефонов</returns>
+        public static List<string> FindPhoneNumbers(string text)
+        {
+            List<string> phoneNumbers = new List<string>();
+            Regex regex = new Regex(@"\+?\d+ ?\(\d+\) ?\d+-?\d+-?\d+");
+
+            MatchCollection matches = regex.Matches(text);
+            foreach (Match match in matches)
+            {
+                phoneNumbers.Add(match.Value);
+            }
+
+            return phoneNumbers;
+        }
+
+        /// <summary>
+        /// Записывает список номеров телефонов в файл
+        /// </summary>
+        /// <param name="phoneNumbers">Список номеров телефонов</param>
+        /// <param name="filePath">Путь к файлу для записи</param>
+        public static void WritePhoneNumbersToFile(List<string> phoneNumbers, string filePath)
+        {
+            File.WriteAllLines(filePath, phoneNumbers);
+        }
+    }
+}
---
+++ HomeWork3/HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,39 @@
+﻿using HomeWork3;
+
+/// <summary>
+/// Главный класс программы
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Главный метод программы
+    /// </summary>
+    /// <param name="args">Аргументы командной строки</param>
+    static void Main(string[] args)
+    {
+        // Расчет средней длины слова
+        var text = "Это какой то тект, за счет которого, будет высчитываться средняя длина слова";
+        var averageWordLength = AverageLength.CalculateAverageLength(text);
+        Console.WriteLine($"Средняя длина слов: {averageWordLength}");
+
+        // Удвоение символов в строке
+        var originalString = "omg i love shrek";
+        var addeParameters = "o kek";
+        var doubledString = DoubleParams.DoubleParameters(originalString, addeParameters);
+        Console.WriteLine($"\nСоединение параметров: {doubledString}");
+
+        // Сложение больших чисел
+        var number1 = "123456789124658769312345678901234567890";
+        var number2 = "9876987654767827293716768213355778679";
+        var sum = LargeNumber.LargeNumbers(number1, number2);
+        Console.WriteLine($"\nСумма больших чисел: {sum}");
+
+        // Переворот слов в строке
+        var sentence = "Иногда само понятие принципов тебе важнее, чем жизнь по ним.";
+        var reversedWords = Reverse.ReverseWords(sentence);
+        Console.WriteLine($"\nРазворот слов: {reversedWords}");
+
+        //Поиск номера телефона
+        FindNumbers.FindAndWritePhoneNumbers();
+    }
+}
