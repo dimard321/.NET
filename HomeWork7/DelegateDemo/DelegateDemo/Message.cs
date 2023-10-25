3e1acadf20e44fcfeaf94768d869b07f44c7d82f
AlexArdas <asg270901@mail.ru>
2023-10-25 18:49:12 +0300

    Выполнил дз 7

---
+++ HomeWork7/DelegateDemo/DelegateDemo/Message.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace DelegateDemo
+{
+    /// <summary>
+    /// Класс, представляющий сообщение, которое отправляется подписчикам объекта Countdown.
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Текст сообщения.
+        /// </summary>
+        public string MyMessage { get; set; }
+
+        /// <summary>
+        /// Конструктор класса Message, инициализирующий текст сообщения с текущей датой и временем.
+        /// </summary>
+        public Message()
+        {
+            MyMessage += DateTime.Now.ToString(CultureInfo.InvariantCulture);
+
+            MyMessage += "Message for subscribes";
+        }
+    }
+}
---
+++ HomeWork7/DelegateDemo/DelegateDemo/SortType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace DelegateDemo
+{
+    /// <summary>
+    /// Перечисление, представляющее тип сортировки.
+    /// </summary>
+    enum SortType
+    {
+        /// <summary>
+        /// Сортировка в порядке возрастания.
+        /// </summary>
+        [Description("Сортировка по возрастанию")]
+        ASC,
+
+        /// <summary>
+        /// Сортировка в порядке убывания.
+        /// </summary>
+        [Description("Сортировка по убыванию")]
+        DESC
+    }
+}
